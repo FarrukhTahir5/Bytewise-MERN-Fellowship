1a4178a8259c602442fb711790959f7cf2e04af0
FarrukhTahir5 <farrukhtahir5@gmail.com>
2023-04-01 15:36:36 +0500

    Performing Taks

---
+++ Task-8_JS-DOM-Manipulation/app.js
@@ -0,0 +1,48 @@
+// // const title= document.querySelector('#main-heading');
+// // title.style.color='red';
+// // console.log(title);
+// // const Tiddies=document.querySelectorAll('td');
+// // Tiddies.forEach(TDy => {
+// //     TDy.style.fontSize='2rem';
+// // });
+
+// // console.log(Tiddies);
+
+// // // Creating Elements
+// // const Table=document.querySelector('tbody');
+// // const Tr=document.createElement('tr');
+// // Table.append(Tr);
+// // // Modifying Text
+// // Tr.innerText="hello";
+
+// // //Modifying Attributes
+// // Tr.setAttribute('class','footer');
+// // Tr.removeAttribute('class');
+
+// // Tr.classList.add('footer');
+// // console.log(Tr.classList.contains('footer'));
+
+// // //remove Element
+
+// // Tr.remove();
+
+// let ul=document.querySelector('ul');
+// // console.log(ul.parentNode.parentNode.parentNode);
+// // console.log(ul.parentElement.parentElement.parentElement);
+// // const html=document.documentElement;
+// // console.log(html.parentNode);
+// // console.log(html.parentElement);
+// // console.log(ul.childNodes);
+// // console.log(ul.firstChild);
+// // console.log(ul.lastChild);
+
+// // ul.childNodes[3].style.backgroundColor='blue';
+// // console.log(ul.children);
+// // console.log(ul.firstElementChild);
+// // console.log(ul.lastElementChild);
+
+// const div=document.querySelector('div');
+
+
+// console.log(ul.previousElementSibling);
+// console.log(ul.nextElementSibling);
