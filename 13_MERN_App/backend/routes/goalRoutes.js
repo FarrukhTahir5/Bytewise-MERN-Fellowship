f0d8d19eba81e65467f343fa7e5c86a0faff9719
FarrukhTahir5 <farrukhtahir5@gmail.com>
2023-06-06 20:48:07 +0500

    completed

--- 13_MERN_App/backend/routes/goalRoutes.js
+++ 13_MERN_App/backend/routes/goalRoutes.js
@@ -1,13 +1,15 @@
-const express=require('express')
-const router=express.Router()
-const {getGoals, setGoals, updateGoals, deleteGoals}=require('../controller/goalController')
-const {protect}=require('../middleware/authMiddleware')
-router.route('/').get(protect,getGoals).post(protect,setGoals)
-router.route('/:id').put(protect,updateGoals).delete(protect,deleteGoals)
+const express = require('express')
+const router = express.Router()
+const {
+  getGoals,
+  setGoal,
+  updateGoal,
+  deleteGoal,
+} = require('../controllers/goalController')
 
-// router.get('/',getGoals)
-// router.post('/',setGoals)
-// router.put('/:id',updateGoals)
-// router.delete('/:id',deleteGoals)
+const { protect } = require('../middleware/authMiddleware')
 
-module.exports=router
+router.route('/').get(protect, getGoals).post(protect, setGoal)
+router.route('/:id').delete(protect, deleteGoal).put(protect, updateGoal)
+
+module.exports = router
--- 13_MERN_App/frontend/src/components/Spinner.jsx
+++ 13_MERN_App/frontend/src/components/Spinner.jsx
@@ -1,6 +1,8 @@
 function Spinner() {
   return (
-    <div className="loadingSpinnerContainer"><div className="loadingSpinner"></div></div>
+    <div className='loadingSpinnerContainer'>
+      <div className='loadingSpinner'></div>
+    </div>
   )
 }
 
